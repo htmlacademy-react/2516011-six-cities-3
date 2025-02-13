5d4a01073b401dcaf6c61940d3fac086151a93c3
bertruchio <mr.2003.artem@gmail.com>
2025-02-14 00:08:40 +0300

    refactor(offer-gallery): временно задаём key контейнерам изображений в виде url на них

--- src/components/offer/offer-gallery/offer-gallery.tsx
+++ src/components/offer/offer-gallery/offer-gallery.tsx
@@ -6,8 +6,8 @@ function OfferGallery({ images }: OfferGalleryProps) {
   return (
     <div className="offer__gallery-container container">
       <div className="offer__gallery">
-        {images.map((image, index) => (
-          <div className="offer__image-wrapper" key={index}>
+        {images.map((image) => (
+          <div className="offer__image-wrapper" key={image}>
             <img className="offer__image" src={image} alt="Photo studio" />
           </div>
         ))}
