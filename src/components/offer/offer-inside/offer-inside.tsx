c5dd3187950750ba7c9bd9c1c7995dab51fb8b45
bertruchio <mr.2003.artem@gmail.com>
2025-02-14 00:21:59 +0300

    refactor(offer-inside): временно задаём key элементам списка в виде строки названия товара

--- src/components/offer/offer-inside/offer-inside.tsx
+++ src/components/offer/offer-inside/offer-inside.tsx
@@ -7,8 +7,8 @@ function OfferInside({ goods }: OfferInsideProps) {
     <div className="offer__inside">
       <h2 className="offer__inside-title">What&apos;s inside</h2>
       <ul className="offer__inside-list">
-        {goods.map((good, index) => (
-          <li className="offer__inside-item" key={index}>{good}</li>
+        {goods.map((good) => (
+          <li className="offer__inside-item" key={good}>{good}</li>
         ))}
       </ul>
     </div>
