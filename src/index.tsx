3a7e90c519464a9e07584d528a54acdbfd96835c
bertruchio <mr.2003.artem@gmail.com>
2025-04-23 01:47:07 +0300

    feat(index): подключит redux к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { Provider } from 'react-redux';
 import { favoritePlaces } from './mocks/favorite-places.ts';
 import { fullOffers } from './mocks/fullOffers.ts';
 import { offers } from './mocks/offers.ts';
 import { reviews } from './mocks/reviews.ts';
+import { store } from './store';
 
 import { RENTAL_OFFERS_COUNT } from './utils/const';
 
@@ -14,12 +16,14 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      rentalOffersCount={RENTAL_OFFERS_COUNT}
-      offers={offers}
-      favoritePlaces={favoritePlaces}
-      fullOffers={fullOffers}
-      reviews={reviews}
-    />
+    <Provider store={store}>
+      <App
+        rentalOffersCount={RENTAL_OFFERS_COUNT}
+        offers={offers}
+        favoritePlaces={favoritePlaces}
+        fullOffers={fullOffers}
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>
 );
