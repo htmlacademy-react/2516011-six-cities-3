ab1b0ea72cb2ee3c2ceb1600fcad6bf2891ca89d
bertruchio <mr.2003.artem@gmail.com>
2025-04-26 02:27:07 +0300

    feat(error-message): cоздаст компонент для отображения ошибки и подключит его

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { favoritePlaces } from './mocks/favorite-places.ts';
 import { fullOffers } from './mocks/fullOffers.ts';
 import { reviews } from './mocks/reviews.ts';
 import { store } from './store';
+import ErrorMessage from './components/error-message/error-message.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,6 +15,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <ErrorMessage />
       <App
         favoritePlaces={favoritePlaces}
         fullOffers={fullOffers}
