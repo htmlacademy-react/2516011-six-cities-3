e1ea87ee8696104e8d37b2eb7b616d0d3ca3b72c
bertruchio <mr.2003.artem@gmail.com>
2025-05-02 22:36:00 +0300

    refactor(map): добавит в компонент карты необязательный параметр текущей точки маршрута

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,15 +1,28 @@
 import { useEffect, useRef, MutableRefObject } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { OfferShort, CityLocation } from '../../types/offer';
+import { CityLocation, BaseOffer } from '../../types/offer';
 import { useMap } from '../../hooks/use-map.tsx';
 
 interface MapProps {
-  offers: OfferShort[];
+  offers: BaseOffer[];
   cityLocation: CityLocation;
+  currentOffer?: BaseOffer;
 }
 
-function Map({ offers, cityLocation }: MapProps) {
+const defaultIcon = new L.Icon({
+  iconUrl: 'img/pin.svg',
+  iconSize: [30, 40],
+  iconAnchor: [15, 40],
+});
+
+const currentIcon = new L.Icon({
+  iconUrl: 'img/pin-active.svg',
+  iconSize: [30, 40],
+  iconAnchor: [15, 40],
+});
+
+function Map({ offers, cityLocation, currentOffer }: MapProps) {
   const mapRef = useRef<HTMLDivElement>(null);
   const mapInstanceRef = useMap(mapRef as MutableRefObject<HTMLElement>, cityLocation);
   const markersRef = useRef<L.Marker[]>([]);
@@ -29,13 +42,21 @@ function Map({ offers, cityLocation }: MapProps) {
 
       offers.forEach((offer) => {
         const { latitude, longitude } = offer.location;
-        const marker = L.marker([latitude, longitude])
+        const marker = L.marker([latitude, longitude], { icon: defaultIcon })
           .addTo(map)
           .bindPopup(`<b>${offer.title}</b>`);
         markersRef.current.push(marker);
       });
+
+      if (currentOffer) {
+        const { latitude, longitude } = currentOffer.location;
+        const marker = L.marker([latitude, longitude], { icon: currentIcon })
+          .addTo(map)
+          .bindPopup(`<b>${currentOffer.title}</b>`);
+        markersRef.current.push(marker);
+      }
     }
-  }, [offers, mapInstanceRef]);
+  }, [offers, currentOffer, mapInstanceRef]);
 
   return <section className="cities__map map" ref={mapRef} style={{ height: '100%' }} />;
 }
