931d7e721b88a94552999aa72af6415d2d525f1b
bertruchio <mr.2003.artem@gmail.com>
2025-05-28 23:35:03 +0300

    refactor(favorite): вынес вычисление количества избранных в селектор

--- src/store/favorite/selectors.ts
+++ src/store/favorite/selectors.ts
@@ -4,3 +4,4 @@ import { NameSpace } from '../../utils/const';
 export const getFavorites = (state: State) => state[NameSpace.Favorite].favorites;
 export const getIsFavoritesLoading = (state: State) => state[NameSpace.Favorite].isFavoritesLoading;
 export const getHasFavoritesBeenLoaded = (state: State): boolean => state[NameSpace.Favorite].hasFavoritesBeenLoaded;
+export const getFavoriteCount = (state: State): number => state[NameSpace.Favorite].favorites.length;
